32337e948676151c36f9be92d4af400602e99a61
Paranitharan <paranitharanparani13@gmail.com>
2024-02-26 11:57:43 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-var data = "Paranitharan"
+
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
